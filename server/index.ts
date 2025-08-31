28393d2e9636effb704edc688c3f83548963db1e
sudhesh.holla <sudheshholla15@gmail.com>
2025-08-31 12:49:03 +0530

    railway fix-2

--- server/index.ts
+++ server/index.ts
@@ -63,11 +63,12 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
+  console.log("Starting server on port", port);
   server.listen({
     port,
     host: "0.0.0.0",
     reusePort: true,
   }, () => {
-    log(`serving on port ${port}`);
+    console.log("🚀 Express listening on port", port);
   });
 })();
